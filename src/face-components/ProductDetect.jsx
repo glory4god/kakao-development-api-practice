91f25c8c130830b5269a116fcfa37d2b71435393
Hayoung <gkduddy0814@gmail.com>
2021-05-18 14:02:59 +0900

    feat : separate components

--- src/face-components/ProductDetect.jsx
+++ src/face-components/ProductDetect.jsx
@@ -2,10 +2,11 @@
 import React from 'react';
 import styled from 'styled-components';
 import qs from 'querystring';
+import ProductInfo from './ProductInfo';
 
 const Root = styled.section`
   .image-grid {
-    position: absolute;
+    position: relative;
     z-index: 1;
   }
 
@@ -80,31 +81,14 @@ const ProductDetect = () => {
         reset
       </button>
       <div className="image-grid">
-        {product.map((item, key) => (
-          <div
-            style={{
-              zIndex: '2',
-              position: 'absolute',
-              height: `${size.height * (item.y2 - item.y1)}px`,
-              width: `${size.width * (item.x2 - item.x1)}px`,
-              top: `${100 * item.y1}%`,
-              left: `${100 * item.x1}%`,
-              border: '2px solid white',
-              color: 'white',
-              fontSize: '0.8rem',
-            }}>
-            <span style={{ backgroundColor: 'black' }}>{item.class}</span>
-          </div>
-        ))}
+        <ProductInfo product={product} size={size} />
         {loading && (
-          <div>
-            <img
-              className="image"
-              src={imageUrl.image_url}
-              alt="image"
-              width="100%"
-            />
-          </div>
+          <img
+            className="image"
+            src={imageUrl.image_url}
+            alt="image"
+            width="100%"
+          />
         )}
       </div>
     </Root>
