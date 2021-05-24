f404c30a1459fd9a25f615de0175b918d333ab0f
Hayoung <gkduddy0814@gmail.com>
2021-05-24 23:38:08 +0900

    feat : get data from api

--- src/guestbook-components/GuestBookMain.jsx
+++ src/guestbook-components/GuestBookMain.jsx
@@ -2,28 +2,43 @@ import React from 'react';
 import styled from 'styled-components';
 
 const Root = styled.div`
-  background-color: black;
-  color: white;
+  .title {
+    font-size: 2rem;
+  }
 `;
 
 const GuestBookMain = () => {
   const [data, setData] = React.useState([]);
+  const [loading, setLoading] = React.useState(false);
+
   const getData = async () => {
+    setLoading(true);
     try {
       const response = await fetch('https://youngari.kay.kr/api/board');
-      const resJson = await JSON.stringify(response);
+      const resJson = await response.json();
       setData(resJson);
+      console.log(response);
+      console.log(resJson);
     } catch (err) {
     } finally {
+      setLoading(false);
     }
   };
+
   React.useEffect(() => {
     getData();
   }, []);
   return (
     <Root>
-      <div>GUESTBOOK</div>
-      <div>{data}</div>
+      <div className="title">GUESTBOOK</div>
+      <div>{loading && <>loading...</>}</div>
+      {data.map((arr, idx) => {
+        return (
+          <div key={arr._id}>
+            {idx + 1} : {arr.name} / {arr.title}
+          </div>
+        );
+      })}
     </Root>
   );
 };
